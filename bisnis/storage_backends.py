d5316a4fbf26214db5b7dbfd377d7d64001c860b
adityaputra99983 <adityaputra@gmail.com>
2026-07-01 10:31:48 +0700

    update fitur

--- bisnis/storage_backends.py
+++
@@ -1,6 +0,0 @@
-from decouple import config
-from storages.backends.s3boto3 import S3Boto3Storage
-
-
-class SupabaseStorageBucket2(S3Boto3Storage):
-    bucket_name = config('SUPABASE_STORAGE_BUCKET_2', default='media-v2')
